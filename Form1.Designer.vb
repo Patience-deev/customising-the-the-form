2bd75cb2842e0a0be3e8644d0cac6273c3ae5cb5
Dell-User <Dell-User@DESKTOP-78SJPVI>
2023-10-03 10:53:20 +0200

    Add project files.

---
+++ Form1.Designer.vb
@@ -0,0 +1,61 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        btnClose = New Button()
+        btnPatienceSedibeng = New Button()
+        SuspendLayout()
+        ' 
+        ' btnClose
+        ' 
+        btnClose.Location = New Point(362, 224)
+        btnClose.Name = "btnClose"
+        btnClose.Size = New Size(75, 23)
+        btnClose.TabIndex = 0
+        btnClose.Text = "close"
+        btnClose.UseVisualStyleBackColor = True
+        ' 
+        ' btnPatienceSedibeng
+        ' 
+        btnPatienceSedibeng.Location = New Point(343, 79)
+        btnPatienceSedibeng.Name = "btnPatienceSedibeng"
+        btnPatienceSedibeng.Size = New Size(191, 40)
+        btnPatienceSedibeng.TabIndex = 1
+        btnPatienceSedibeng.Text = "Patience Sedibeng"
+        btnPatienceSedibeng.UseVisualStyleBackColor = True
+        ' 
+        ' Form1
+        ' 
+        AutoScaleDimensions = New SizeF(7F, 15F)
+        AutoScaleMode = AutoScaleMode.Font
+        ClientSize = New Size(800, 450)
+        Controls.Add(btnPatienceSedibeng)
+        Controls.Add(btnClose)
+        Name = "Form1"
+        Text = "Form1"
+        ResumeLayout(False)
+    End Sub
+
+    Friend WithEvents btnClose As Button
+    Friend WithEvents btnPatienceSedibeng As Button
+End Class
---
+++ Form1.vb
@@ -0,0 +1,19 @@
+﻿Public Class Form1
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Me.Text = "My first VB2022 App"
+        Me.BackColor = Color.Blue
+        Me.MaximizeBox = True
+        Me.MinimizeBox = True
+        Me.Size = New Size(800, 400)
+        Me.Opacity = 0.85
+        Me.CenterToParent()
+
+    End Sub
+    Private Sub btnClose_Click(sender As Object, e As EventArgs) Handles btnClose.Click
+        Close()
+
+    End Sub
+    Private Sub button1_Click(Sender As Object, e As EventArgs) Handles btnPatienceSedibeng.Click
+        MsgBox("This is Patience")
+    End Sub
+End Class
